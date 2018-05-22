3f062703e567ac3e2bc9bba3fce5c03e74730110
Marwa Elali <marwaelali@gmail.com>
2018-05-22 14:39:49 +0100

    added simplecov gem for test coverage

--- Gemfile
+++ Gemfile
@@ -4,11 +4,10 @@ source 'https://rubygems.org'
 
 ruby '2.5.0'
 
-group :test do
-  gem 'rspec'
-end
-
+gem 'rspec'
 gem 'rubocop'
+gem 'simplecov', require: false, group: :test
+gem 'simplecov-console', require: false, group: :test
 gem 'terminal-table'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
